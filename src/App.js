af59d04fb4bf40f921f054da89843c164d66c9bd
akramulratul <68425589+akramulratul@users.noreply.github.com>
2021-12-08 01:54:09 +0600

    registration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Home from "./Component/Home/Home";
+import LogIn from "./Component/Login/LogIn";
+import Registration from "./Component/Registration/Registration";
+import Header from "./Component/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+          <Route path="/registration">
+            <Registration />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Component/Registration/Registration.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Registration = () => {
+  return (
+    <div className="container">
+      <h1>Please Register</h1>
+    </div>
+  );
+};
+
+export default Registration;
